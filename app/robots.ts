92b4efcf14ca32263f0374c0bc318b5c2ccc336e
Abhay241-ds <pratapsinghabhay650@gmail.com>
2026-09-06 21:13:26 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://gloss-glow-one.vercel.app/";
+const baseUrl = "https://gloss-glow-one.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {
   return {
